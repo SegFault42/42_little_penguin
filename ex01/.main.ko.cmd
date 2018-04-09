1f04780cd7c75b6aa1e8a218947ba9bcb0724acd
SegFault42 <rabougue@student.42.fr>
2018-04-09 20:46:05 +0000

    ex02 ok

--- ex01/.main.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/segfault42/Documents/42_little_penguin/ex01/main.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/segfault42/Documents/42_little_penguin/ex01/main.ko /home/segfault42/Documents/42_little_penguin/ex01/main.o /home/segfault42/Documents/42_little_penguin/ex01/main.mod.o ;  true
--- ex01/.tmp_versions/main.mod
+++
@@ -1,3 +0,0 @@
-/home/segfault42/Documents/42_little_penguin/ex01/main.ko
-/home/segfault42/Documents/42_little_penguin/ex01/main.o
-
