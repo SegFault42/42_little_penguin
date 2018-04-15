2272320493c6d6e1930ce6d20651ac671d37f748
SegFault42 <rabougue@student.42.fr>
2018-04-15 00:19:23 +0000

    ex05 open release ok

--- ex05/main.c
+++ ex05/main.c
@@ -10,73 +10,36 @@ MODULE_AUTHOR("SegFault42 <SegFault42@protonmail.com>");
 MODULE_DESCRIPTION("Misc device module");
 MODULE_LICENSE("GPL");
 
-static int	dev_open(struct inode *, struct file *);
-static int	dev_release(struct inode *, struct file *);
-static ssize_t	dev_write(struct file *, const char *, size_t, loff_t *);
-static ssize_t	dev_read(struct file *, char *, size_t, loff_t *);
+static int	misc_open(struct inode *inode, struct file *filp);
+static int	misc_release(struct inode *inode, struct file *filp);
 
 static struct miscdevice	misc_device;
-static int			times = 0;
-static char			msg[100] = {0};
-static short			readPos = 0;
-
-static struct file_operations	fops =
-{
-	.open = dev_open,
-	.read = dev_read,
-	.write = dev_write,
-	.release = dev_release,
+static struct file_operations f_ops = {
+	.open = misc_open,
+	.release = misc_release,
 };
+static int	incr = 0;
 
-static int	dev_open(struct inode *inod, struct file *fi)
+
+static int	misc_open(struct inode *inode, struct file *filp)
 {
-	times++;
-	pr_info("Device opened %d times\n", times);
+	incr++;
 	return 0;
 }
 
-static int	dev_release(struct inode *inod, struct file *fi)
+static int	misc_release(struct inode *inode, struct file *filp)
 {
-	pr_info("Device closed\n");
+	incr--;
 	return 0;
-}
-static ssize_t	dev_read(struct file *filp, char *buff, size_t len, loff_t *off)
-{
-	short	count = 0;
-
-	while (len && (msg[readPos] != 0))
-	{
-		put_user(msg[readPos], buff++);
-		count++;
-		len --;
-		readPos++;
-	}
-	return count;
-}
-
-static ssize_t	dev_write(struct file *filp, const char *buff, size_t len, loff_t *off)
-{
-	short	ind = len -1;
-	short	count = 0;
-
-	memset(msg, 0, 100);
-	readPos = 0;
-	while (len > 0)
-	{
-		msg[count++] = buff[ind--];
-		len--;
-	}
-	return count;
 }
 
 int	create_misc(void)
 {
 	int			retval;
-	struct file_operations	fops;
 
 	misc_device.minor = MISC_DYNAMIC_MINOR;
 	misc_device.name = "fortytwo";
-	misc_device.fops = &fops;
+	misc_device.fops = &f_ops;
 	retval = misc_register(&misc_device);
 	if (retval == -1) {
 		pr_err("misc_register() failure\n");
