f7eeabe5d8240e2ee4ee636470323de791a92b12
SegFault42 <rabougue@student.42.fr>
2018-05-07 22:08:06 +0000

    seem ok

--- ex09/main.c
+++ ex09/main.c
@@ -1,25 +1,57 @@
 #include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/dcache.h>
-#include "/sources/linux-4.15.3/fs/proc/internal.h"
+#include <linux/proc_fs.h>
+#include <linux/fs_struct.h>
+#include <linux/mount.h>
+#include <linux/string.h>
+#include <linux/uaccess.h>
+
+#define PROCFS_NAME	"mymounts"
+#define BUFF_SIZE	256
+
+char		buffer[BUFF_SIZE] = {0};
 
 static struct	proc_dir_entry	*proc_entry;
 
+static ssize_t	read_proc(struct file *filp, char *buf, size_t len, loff_t *offp);
+
 static const struct file_operations fops = {
 	.owner = THIS_MODULE,
+	.read = read_proc,
 };
-
-static ssize_t	read_proc(struct file *filp, char *buf, size_t len, loff_t *offp);
 
 static ssize_t	read_proc(struct file *filp, char *buf, size_t len, loff_t *offp)
 {
+	struct dentry	*curdentry;
+
+	list_for_each_entry(curdentry, &current->fs->root.mnt->mnt_root->d_subdirs, d_child)
+	{
+		if (curdentry->d_flags & DCACHE_MOUNTED) {
+			strcat(buffer, curdentry->d_name.name);
+			strcat(buffer, "\t");
+			strcat(buffer, curdentry->d_sb->s_root->d_name.name);
+			strcat(buffer, curdentry->d_name.name);
+			strcat(buffer, "\n");
+		}
+	}
+	if (*offp == 0 && len > 5) {
+		if (copy_to_user(buf, buffer, strlen(buffer)) != 0) {
+			pr_info("copy_to_user() failure");
+			return -1;
+		}
+		*offp = strlen(buffer);
+		return strlen(buffer);
+	}
 	return 0;
+
 }
 
 static int __init begin(void)
 {
-	proc_entry = proc_create("mymounts", 0, NULL, &fops);
+	proc_entry = proc_create(PROCFS_NAME, 0, NULL, &fops);
 	if (proc_entry == NULL) {
 		pr_alert("proc_create() failure\n");
 		return -ENOMEM;
@@ -30,7 +62,7 @@ static int __init begin(void)
 
 static void __exit end(void)
 {
-	remove_proc_entry("mymounts", NULL);
+	remove_proc_entry(PROCFS_NAME, NULL);
 	pr_info("remove_proc_entry() Success\n");
 }
 
